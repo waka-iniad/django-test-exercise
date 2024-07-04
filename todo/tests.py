193894fdf8b73b56ed436c04bac4831e9789c565
1F10230043 <s1f102300436@iniad.org>
2024-07-04 15:35:02 +0900

    修正2

--- todo/tests.py
+++ todo/tests.py
@@ -71,7 +71,7 @@ class TodoViewTestCase(TestCase):
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datatime(2024, 8, 1)))
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=due')
